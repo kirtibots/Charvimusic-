e95405c48f9e2b43b337239b6ef1f6f3874d48f2
- ᴋɪʀᴛɪ ʙоᴛѕ [ 🇮🇳 <kritikumari8665@gmail.com>
2026-07-12 07:10:29 +0530

    Import InlineKeyboardButton and InlineKeyboardMarkup

--- REVANGEMUSIC/plugins/admins/callback.py
+++ REVANGEMUSIC/plugins/admins/callback.py
@@ -7,7 +7,7 @@
 # =======================================================
 
 import asyncio
-from telegram import CallbackQuery
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
